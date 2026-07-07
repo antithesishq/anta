414dfecd1dea036a2eab3f5f06e4451fc64f5b76
Kseniia Toloknova <kseniatolokn@gmail.com>
2026-07-07 13:02:53 -0500

    Revert "refactor: remove size-specific padding override for small input"
    This reverts commit 217b86e1fdd848ff89484e96527c2ddd26a9ee50.

--- src/elements/a-input.ts
+++ src/elements/a-input.ts
@@ -165,6 +165,8 @@ const SHADOW_STYLE = `
   :host([size="large"]) { --_fs: 17px; --_lh: 22px; }
   :host([size="small"]) .field { min-height: 24px; }
   :host([size="large"]) .field { min-height: 32px; }
+  :host([size="small"]) input,
+  :host([size="small"]) textarea { padding-inline: 5px; }
   :host([round]) .field { border-radius: var(--input-round, 999px); }
 
   @media (hover: hover) and (pointer: fine) {
